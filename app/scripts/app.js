7163a20ff4a69b1c8332d000c63466d7b8b104f1
madebyrag <madebyrag@qq.com>
2015-11-30 15:08:34 +0800

    All: version 0.0.1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,10 +21,10 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/body', {
+        templateUrl: 'views/body.html'
+        //controller: 'AboutCtrl',
+          // controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
