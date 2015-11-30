49e189645139250a84f9423d948fa87e74b9d4e6
madebyrag <madebyrag@qq.com>
2015-11-30 11:12:34 +0800

    All: init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name junyusSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the junyusSiteApp
+ */
+angular.module('junyusSiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
