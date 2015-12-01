71134cbb9bf29e5ca5336a43057ae910ff79f5bf
madebyrag <madebyrag@qq.com>
2015-12-01 21:18:57 +0800

    Main: works finished

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,24 +9,69 @@
  * Main module of the application.
  */
 angular
-  .module('junyusSiteApp', [
-    'ngAnimate',
-    'ngRoute',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/body', {
-        templateUrl: 'views/body.html'
-        //controller: 'AboutCtrl',
-          // controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('junyusSiteApp', [
+        'ngAnimate',
+        'ngRoute',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/body', {
+                templateUrl: 'views/body.html',
+                controller: 'worksLayout',
+                // controllerAs: 'about'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('worksLayout', function($scope) {
+        $scope.worksData = [
+            {
+                left: ["../images/main/Hummer.png"],
+                right: {
+                    title: 'Hummer',
+                    content: 'Hummer is a game application that aims to help people' +
+                    'sing better.In the game, players sing through the microphone to control' +
+                    'a hummingbird and navigate through adventurous terrains.'
+                }
+            },
+            {
+                left: ["../images/main/OneArm.png"],
+                right: {
+                    title: 'OneArm',
+                    content: 'OneArm is a wearable armband  that allows people control computer' +
+                    ' using gesture control. With the software client, users are able to take control' +
+                    ' of applications on computer or other devices wit h five basic gestures ' +
+                    'and their specific combinations'
+                }
+            },
+            {
+                left: [
+                    "../images/main/Headring.png",
+                    "../images/main/Tracker.png"
+                ],
+                right: {
+                    title: 'Visualizing Headring',
+                    content: 'Visualizing Headring is a wearable auxiliary product that aids the blind to ' +
+                    'avoid obstacles and navigate to a given destination. With vibration from the headring' +
+                    ' and voice navigation from the mobile application, users are guided within the given' +
+                    ' route to the destination. '
+                }
+            },
+            {
+                left: ["../images/main/OverHUSTpng.png"],
+                right: {
+                    title: 'OverHust',
+                    content: 'OverHust is a mobile application that utilizes street view maps to help users ' +
+                    'navigate routines on campus.With highly detailed panoramic street-level photos, users are' +
+                    ' better able to find the destinations,or just take a visual tour on campus within the app. '
+                }
+            }
+        ];
+    });
